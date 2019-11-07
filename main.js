252bff91cf39c33208a11fab5ff958d293e634b6
Iyobosa Jefferson Osagie <iyobosajefferson2@gmail.com>
2019-11-07 10:06:44 +0100

    Redesigned the label creator widget

--- main.js
+++ main.js
@@ -1,104 +1,110 @@
 // DOM variables
 
+var  form = document.querySelector("form");
 var  input = document.querySelector("#collect");
-var  basket = document.querySelector(".archive");
-var  hook = document.querySelector("button");
-var  alert = document.querySelector(".message");
-
+var  container = document.querySelector(".archive");
+var  submit = document.querySelector("button");
+var  message = document.querySelector(".message");
 // Adding event to the Add Label button
 
-hook.addEventListener("click", autoLabel);
-var array = [];
+// hook.addEventListener("click", autoLabel);
 
+// Array to hold the labels entered for backend export
+var labelsForExport = [];
 
-// Array of messages
+// To manipulate the DOM
+var labelsElements = [];
 
-	var messages = {
-				invalid: "Please enter a value!",
-				hint: "Try entering words that start with vowels and see how the labels are displayed.",
-				info: "It also makes sure that empty spaces are not submitted"
-				};
+// Process the value from the input
+function processInput(tag){
+	let inputValue = tag.value;
+	// Get rid of white space
+	let trimmedTag = inputValue.trim();
 
-	// Hide hints
-	function hideInfo(){
-		setTimeout(hideAlert, 2500);
-
-		function hideAlert(){
-			alert.style.top = "10px";
-			// Give the alert a little stretch effect before leaving the viewport
-			setTimeout(animateAlert, 200)
-			function animateAlert(){
-				alert.style.top = "-200px";
-			}
-		}
-	};
-
-
-	// Waring display
-	function showAlert(messageToShow){
-		alert.innerText = messageToShow;
-		alert.style.top = "0px";
-		hideInfo();
-	}
-
-
-// Main function to do the labels
-
-function autoLabel(){
-
-	// Variables for processing arrays
-
-	let trimmed = input.value.trim();
-	let processed = trimmed.split(".");
-	array.push(processed);
-	var text;
-	var label = document.createElement("span");
-	var form = document.querySelector("form");
-	var inputValue = input.value;
-
-	// Preventing reloading of page
-		form.addEventListener("submit", function(event){
-		event.preventDefault()
-	})
-
-
-
-
-
-
-
-
-	// Returning the value entered to be the text of the label created
-	for (var i = 0; i < array.length; i++) {
-		label;
-		label.innerText = array[i];
-	}
-
-	// Making sure empty spaces aren't submitted
-	if (inputValue.trim() == "") {
-		showAlert(messages.invalid);
-	}else{
-		if (label.innerText[0].toLowerCase() == "a" ) {
-			label.style.background = "green";
-		}
-		else if (label.innerText[0].toLowerCase() == "e" ) {
-			label.style.background = "red";
-		}
-		else if (label.innerText[0].toLowerCase() == "i" ) {
-			label.style.background = "blue";
-		}
-		else if (label.innerText[0].toLowerCase() == "o" ) {
-			label.style.background = "red";
-		}
-		else if (label.innerText[0].toLowerCase() == "u" ) {
-			label.style.background = "#254412";
-		}
-		else{
-			basket.appendChild(label);
-		}
-		basket.appendChild(label);
-	}
-	input.value = "";
-
+	// Send the input to the export array
+	labelsForExport.push(trimmedTag.toLowerCase());
+	return trimmedTag
 };
+
+// Create the labels
+function createTag(tag){
+	// Create label Elements
+	let div = document.createElement("div");
+	let textNode = document.createElement("span");
+	let deleteTag = document.createElement("span");
+
+	// Set elements' properties
+	div.className = "label";
+	textNode.className = 'text';
+	deleteTag.className = "delete-tag";
+
+	textNode.innerText = tag;
+	deleteTag.innerHTML = "&times;";
+	// Append the elements inside the label
+	div.appendChild(deleteTag);
+	div.prepend(textNode);
+	// Add a unique data-attribute to the label
+	div.setAttribute('data-tag', tag);
+	deleteTag.setAttribute('data-target-tag', tag);
+	// Add to the labelsELement array
+	labelsElements.push(div);
+	// return created element
+	return div;
+}
+
+
+// Create tag on button click 
+function newTagInstance(){
+	// Start a new instance of Making the tag
+	let newTag =  processInput(input);
+	let createdTag = createTag(newTag);
+
+	// Append the tag before the input
+	container.insertBefore(createdTag, input);
+	// Reset/clear the input field
+	input.value = "";
+	
+}
+
+submit.addEventListener('click', ()=>{
+	// Preventing reloading of page when submitting
+		form.addEventListener("submit", (e)=>{
+		e.preventDefault();
+	});
+	// Preventing submission of empty input
+	if (input.value == ""  ) {
+		message.innerText = "Please input a value!"
+		message.classList.remove("hidden");
+	}else if( labelsForExport.includes(input.value.toLowerCase()) ){
+		message.classList.remove("hidden");
+		message.innerText = "Label already exists!"
+	}
+	else{
+		message.classList.add("hidden");
+		newTagInstance();
+	}
+	setTimeout(()=>{message.classList.add("hidden")}, 2000)
+});
+
+
+// Delete the particular label when the delete icon on it is clicked
+document.addEventListener("click", deleteTag)
+function deleteTag(e){
+	// Check if the click was on the right tag
+	if (e.target.className == 'delete-tag' ) {
+		let purgeTag = e.target.getAttribute('data-target-tag');
+		// Loop through the array and match the selected label for deletion
+		labelsElements.forEach((label)=>{
+			if (label.getAttribute("data-tag") === purgeTag) {
+				let index = labelsElements.indexOf(label);
+				// Remove and update the array of labels
+				labelsForExport.splice(index, 1);
+				labelsElements.splice(index, 1);
+				// Remove the label from the DOM
+				container.removeChild(label);
+			}
+		})
+	} 
+
+}
 
